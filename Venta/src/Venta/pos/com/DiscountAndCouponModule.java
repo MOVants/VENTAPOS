98d140d03122a9c6828453938c1f1c649abebca6
marvin <marvin@Gnome>
2018-10-24 16:25:13 +0800

    Refactor and added libraries for gecko and chrome

--- Venta/src/Venta/pos/com/DiscountAndCouponModule.java
+++ Venta/src/Venta/pos/com/DiscountAndCouponModule.java
@@ -8,161 +8,173 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DiscountAndCouponModule extends StringValues {
-	  WebDriver driver;
-	  private String baseUrl;
+	static WebDriver driver;
+	static String browser;
 
-	  
-	  @Before
-	  public void setUp() throws Exception {
-//		  System.setProperty("webdriver.gecko.driver","/home/marvin/Documents/Selinium/geckodriver"); 
-//	    driver = new FirefoxDriver();
-		  System.setProperty("webdriver.chrome.driver", "/home/marvin/Documents/Selinium/chromedriver");
-		  driver = new ChromeDriver();
-		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		  driver.manage().window().maximize();
-		  baseUrl = "http://172.16.0.12:8589/#/login";
-		  driver.get(baseUrl);
-	  }
-	  
-	  @Test
-	  public void LoginTest() throws Exception {
-		  toAdmLogin();
-		  System.out.println("Login test = Passed!");
-	  }
-	 
-	  @Test
-	  public void createPWDDiscountTest() throws Exception {
-		  toAdmLogin();
-		  discountAndCouponMngtBtn();
-		  Thread.sleep(1000);
-		  createPWDSeniorDiscBtn();
-		  Thread.sleep(1000);
-		  setPWDSeniorDiscDetails();
-		  saveDiscountDetails();
-		  Thread.sleep(1000);
-		  String str = driver.findElement(By.xpath(getDescountSuccess)).getText();
-		  if (str.contains("Successfully")) {
-			  System.out.println("Create SR/PWD discount test = Passed!");
-		  }else {
-			  System.out.println("Create SR/PWD discount test = Failed!");
-		  }
-		  
-//		  Assert.assertEquals(str, "Successfully created discount.");
-//		  Assert.assertTrue(str.contains("Successfully"));
-//		    if (str.equals("Successfully created discount."))
-//			{
-//				System.out.println("Create SR/PWD discount test = Passed!");
-//			} else {
-//				System.out.println("Create SR/PWD discount test = Failed!");
-//			}
-	  }
-	  
-	  @Test
-	  public void emptyPWDDiscountNameFieldTest() throws Exception{
-		  toAdmLogin();
-		  discountAndCouponMngtBtn();
-		  Thread.sleep(1000);
-		  createPWDSeniorDiscBtn();
-		  Thread.sleep(1000);
-		  setPWDSeniorDiscDetails();
-		  driver.findElement(By.xpath(discountNameField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-		  driver.findElement(By.xpath(discountNameField)).sendKeys(Keys.BACK_SPACE);
-		  saveDiscountDetails();
-		  Thread.sleep(1000);
-		  String str = driver.findElement(By.xpath(getDescountError)).getText();
-		  if (str.contains("Please review fields again.")) {
-			  System.out.println("Empty SR/PWD Discount Name Field Test = Passed!");
-		  }else {
-			  System.out.println("Empty SR/PWD Discount Name Field Test = Failed!");
-		  }
-	  }
-	  
+	public void setBrowser() {
+		browser = "Chrome";
+	}
+	public void browserConfig () {
+
+		if (browser.contains("Firefox")) {
+			System.setProperty("webdriver.gecko.driver","/home/marvin/git/VENTAPOS/Venta/libs/geckoDriver/geckodriver"); 
+			driver = new FirefoxDriver();
+		}else if (browser.contains("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", "/home/marvin/git/VENTAPOS/Venta/libs/chromeDriver/chromedriver");
+			driver = new ChromeDriver();
+		}
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		setBrowser();
+		browserConfig();
+		driver.get("http://172.16.0.12:8589");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 	
-	  @Test
-	  public void emptyPWDDiscountPercentFieldTest() throws Exception{
-		  toAdmLogin();
-		  discountAndCouponMngtBtn();
-		  Thread.sleep(1000);
-		  createPWDSeniorDiscBtn();
-		  Thread.sleep(1000);
-		  setPWDSeniorDiscDetails();
-		  driver.findElement(By.xpath(discountPercentField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-		  driver.findElement(By.xpath(discountPercentField)).sendKeys(Keys.BACK_SPACE);
-		  driver.findElement(By.xpath(discountPercentField)).sendKeys(RandomStringUtils.randomNumeric(2));
-		  saveDiscountDetails();
-		  Thread.sleep(1000);
-		  String str = driver.findElement(By.xpath(getDescountError)).getText();
-		  if (str.contains("Please review fields again.")) {
-				System.out.println("Empty SR/PWD Discount Percent Field Test = Passed!");
-			} else {
-				System.out.println("Empty SR/PWD Discount Percent Field Test = Failed!");
-			}
-	  }
-	  
-	  @Test
-	  public void modifyPWDDiscountTest() throws Exception{
-		  toAdmLogin();
-		  discountAndCouponMngtBtn();
-		  Thread.sleep(1000);
-		  selectPWDSeniorDisctoModify();
-		  String discountName = driver.findElement(By.xpath(discountNameField)).getText();
-		  Thread.sleep(1000);
-		  modifySrPWDDiscountDetails();
-		  
-		  saveDiscountDetails();
-		  Thread.sleep(1000);
-		  String str = driver.findElement(By.xpath(getDescountSuccess)).getText();
-		  if (str.contains(discountName)) {
-				System.out.println("Modify SR/PWD Discount Test = Passed!");
-			} else {
-				System.out.println("Modify SR/PWD Discount Test = Failed!");
-			}
-	  }
 
 	@Test
-	  public void modifyPWDDiscountNameFieldTest() throws Exception{
-		  toAdmLogin();
-		  discountAndCouponMngtBtn();
-		  Thread.sleep(1000);
-		  selectPWDSeniorDisctoModify();
-		  driver.findElement(By.xpath(discountNameField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-		  driver.findElement(By.xpath(discountNameField)).sendKeys(Keys.BACK_SPACE);
-		  driver.findElement(By.xpath(discountNameField)).sendKeys(RandomStringUtils.randomNumeric(8));
-		  String discountName = driver.findElement(By.xpath(discountNameField)).getText();
-		  saveDiscountDetails();
-		  Thread.sleep(1000);
-		  String str = driver.findElement(By.xpath(getDescountSuccess)).getText();
-		  if (str.contains(discountName)) {
-				System.out.println("Modify SR/PWD Discount Name Field  Test = Passed!");
-			} else {
-				System.out.println("Modify SR/PWD Discount Name Field  Test = Failed!");
-			}
-	  }	  
-	
-	  @Test
-	  public void modifyPWDDiscountPercentFieldTest() throws Exception{
-		  toAdmLogin();
-		  discountAndCouponMngtBtn();
-		  Thread.sleep(1000);
-		  selectPWDSeniorDisctoModify();
-		  driver.findElement(By.xpath(discountPercentField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-		  driver.findElement(By.xpath(discountPercentField)).sendKeys(Keys.BACK_SPACE);
-		  driver.findElement(By.xpath(discountPercentField)).sendKeys(RandomStringUtils.randomNumeric(8));
-		  String discountName = driver.findElement(By.xpath(discountNameField)).getText();
-		  saveDiscountDetails();
-		  Thread.sleep(1000);
-		  String str = driver.findElement(By.xpath(getDescountSuccess)).getText();
-		  if (str.contains(discountName)) {
-				System.out.println("Modify SR/PWD Discount Percent Field  Test = Passed!");
-			} else {
-				System.out.println("Modify SR/PWD Discount Percent Field  Test = Failed!");
-			}
-	  }	  
-	  
-	  
-	  
+	public void LoginTest() throws Exception {
+		toAdmLogin();
+		System.out.println("Login test = Passed!");
+	}
+
+	@Test
+	public void createPWDDiscountTest() throws Exception {
+		toAdmLogin();
+		discountAndCouponMngtBtn();
+		Thread.sleep(1000);
+		createPWDSeniorDiscBtn();
+		Thread.sleep(1000);
+		setPWDSeniorDiscDetails();
+		saveDiscountDetails();
+		Thread.sleep(1000);
+		String str = driver.findElement(By.xpath(getDescountSuccess)).getText();
+		if (str.contains("Successfully")) {
+			System.out.println("Create SR/PWD discount test = Passed!");
+		}else {
+			System.out.println("Create SR/PWD discount test = Failed!");
+		}
+
+		//		  Assert.assertEquals(str, "Successfully created discount.");
+		//		  Assert.assertTrue(str.contains("Successfully"));
+		//		    if (str.equals("Successfully created discount."))
+		//			{
+		//				System.out.println("Create SR/PWD discount test = Passed!");
+		//			} else {
+		//				System.out.println("Create SR/PWD discount test = Failed!");
+		//			}
+	}
+
+	@Test
+	public void emptyPWDDiscountNameFieldTest() throws Exception{
+		toAdmLogin();
+		discountAndCouponMngtBtn();
+		Thread.sleep(1000);
+		createPWDSeniorDiscBtn();
+		Thread.sleep(1000);
+		setPWDSeniorDiscDetails();
+		driver.findElement(By.xpath(discountNameField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		driver.findElement(By.xpath(discountNameField)).sendKeys(Keys.BACK_SPACE);
+		saveDiscountDetails();
+		Thread.sleep(1000);
+		String str = driver.findElement(By.xpath(getDescountError)).getText();
+		if (str.contains("Please review fields again.")) {
+			System.out.println("Empty SR/PWD Discount Name Field Test = Passed!");
+		}else {
+			System.out.println("Empty SR/PWD Discount Name Field Test = Failed!");
+		}
+	}
+
+
+	@Test
+	public void emptyPWDDiscountPercentFieldTest() throws Exception{
+		toAdmLogin();
+		discountAndCouponMngtBtn();
+		Thread.sleep(1000);
+		createPWDSeniorDiscBtn();
+		Thread.sleep(1000);
+		setPWDSeniorDiscDetails();
+		driver.findElement(By.xpath(discountPercentField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		driver.findElement(By.xpath(discountPercentField)).sendKeys(Keys.BACK_SPACE);
+		driver.findElement(By.xpath(discountPercentField)).sendKeys(RandomStringUtils.randomNumeric(2));
+		saveDiscountDetails();
+		Thread.sleep(1000);
+		String str = driver.findElement(By.xpath(getDescountError)).getText();
+		if (str.contains("Please review fields again.")) {
+			System.out.println("Empty SR/PWD Discount Percent Field Test = Passed!");
+		} else {
+			System.out.println("Empty SR/PWD Discount Percent Field Test = Failed!");
+		}
+	}
+
+	@Test
+	public void modifyPWDDiscountTest() throws Exception{
+		toAdmLogin();
+		discountAndCouponMngtBtn();
+		Thread.sleep(1000);
+		selectPWDSeniorDisctoModify();
+		String discountName = driver.findElement(By.xpath(discountNameField)).getText();
+		Thread.sleep(1000);
+		modifySrPWDDiscountDetails();
+
+		saveDiscountDetails();
+		Thread.sleep(1000);
+		String str = driver.findElement(By.xpath(getDescountSuccess)).getText();
+		if (str.contains(discountName)) {
+			System.out.println("Modify SR/PWD Discount Test = Passed!");
+		} else {
+			System.out.println("Modify SR/PWD Discount Test = Failed!");
+		}
+	}
+
+	@Test
+	public void modifyPWDDiscountNameFieldTest() throws Exception{
+		toAdmLogin();
+		discountAndCouponMngtBtn();
+		Thread.sleep(1000);
+		selectPWDSeniorDisctoModify();
+		driver.findElement(By.xpath(discountNameField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		driver.findElement(By.xpath(discountNameField)).sendKeys(Keys.BACK_SPACE);
+		driver.findElement(By.xpath(discountNameField)).sendKeys(RandomStringUtils.randomNumeric(8));
+		String discountName = driver.findElement(By.xpath(discountNameField)).getText();
+		saveDiscountDetails();
+		Thread.sleep(1000);
+		String str = driver.findElement(By.xpath(getDescountSuccess)).getText();
+		if (str.contains(discountName)) {
+			System.out.println("Modify SR/PWD Discount Name Field  Test = Passed!");
+		} else {
+			System.out.println("Modify SR/PWD Discount Name Field  Test = Failed!");
+		}
+	}	  
+
+	@Test
+	public void modifyPWDDiscountPercentFieldTest() throws Exception{
+		toAdmLogin();
+		discountAndCouponMngtBtn();
+		Thread.sleep(1000);
+		selectPWDSeniorDisctoModify();
+		driver.findElement(By.xpath(discountPercentField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		driver.findElement(By.xpath(discountPercentField)).sendKeys(Keys.BACK_SPACE);
+		driver.findElement(By.xpath(discountPercentField)).sendKeys(RandomStringUtils.randomNumeric(8));
+		String discountName = driver.findElement(By.xpath(discountNameField)).getText();
+		saveDiscountDetails();
+		Thread.sleep(1000);
+		String str = driver.findElement(By.xpath(getDescountSuccess)).getText();
+		if (str.contains(discountName)) {
+			System.out.println("Modify SR/PWD Discount Percent Field  Test = Passed!");
+		} else {
+			System.out.println("Modify SR/PWD Discount Percent Field  Test = Failed!");
+		}
+	}	  
+
+
+
 	private void saveDiscountDetails() {
 		driver.findElement(By.xpath(savePWDSrDiscountBtn)).click();	
 	}
@@ -171,21 +183,21 @@ public class DiscountAndCouponModule extends StringValues {
 		driver.findElement(By.xpath(discountNameField)).sendKeys(RandomStringUtils.randomAlphanumeric(8));
 		driver.findElement(By.xpath(discountPercentField)).sendKeys(RandomStringUtils.randomNumeric(2));
 	}
-	
-	  
-	  private void selectPWDSeniorDisctoModify() {
+
+
+	private void selectPWDSeniorDisctoModify() {
 		driver.findElement(By.xpath(sRPWDEditBtn)).click();
-		
+
 	}
-	  
-	  private void modifySrPWDDiscountDetails() {
-		  driver.findElement(By.xpath(discountNameField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-		  driver.findElement(By.xpath(discountNameField)).sendKeys(Keys.BACK_SPACE);
-		  driver.findElement(By.xpath(discountNameField)).sendKeys(RandomStringUtils.randomNumeric(2));
-		  driver.findElement(By.xpath(discountPercentField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-		  driver.findElement(By.xpath(discountPercentField)).sendKeys(Keys.BACK_SPACE);
-		  driver.findElement(By.xpath(discountPercentField)).sendKeys(RandomStringUtils.randomNumeric(2));
-		}
+
+	private void modifySrPWDDiscountDetails() {
+		driver.findElement(By.xpath(discountNameField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		driver.findElement(By.xpath(discountNameField)).sendKeys(Keys.BACK_SPACE);
+		driver.findElement(By.xpath(discountNameField)).sendKeys(RandomStringUtils.randomNumeric(2));
+		driver.findElement(By.xpath(discountPercentField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		driver.findElement(By.xpath(discountPercentField)).sendKeys(Keys.BACK_SPACE);
+		driver.findElement(By.xpath(discountPercentField)).sendKeys(RandomStringUtils.randomNumeric(2));
+	}
 
 
 	private void createPWDSeniorDiscBtn() {
@@ -195,16 +207,16 @@ public class DiscountAndCouponModule extends StringValues {
 	private void discountAndCouponMngtBtn() {
 		driver.findElement(By.xpath(discountAndCouponMngtBtn)).click();
 	}
-	
+
 	private void toAdmLogin() {
 		driver.findElement(By.xpath(emailField)).sendKeys(authAdm);
-	    driver.findElement(By.xpath(passwordField)).sendKeys(authAdmPassword);
-	    driver.findElement(By.xpath(btnLogin)).click();
+		driver.findElement(By.xpath(passwordField)).sendKeys(authAdmPassword);
+		driver.findElement(By.xpath(btnLogin)).click();
 	}
 
 	@After
-	  public void tearDown() throws Exception {
-	    driver.quit();
-	 
-	  }
+	public void tearDown() throws Exception {
+		driver.quit();
+
+	}
 }
--- Venta/src/Venta/pos/com/ItemsModule.java
+++ Venta/src/Venta/pos/com/ItemsModule.java
@@ -9,173 +9,175 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.junit.After;
 import org.junit.Before;
 
 public class ItemsModule extends StringValues{
-WebDriver driver;
-String baseUrl;
+	private WebDriver driver;
+	static String browser;
 
-	  
-@Before
-public void setUp() throws Exception {
-//	System.setProperty("webdriver.gecko.driver","/home/marvin/Documents/Selinium/geckodriver"); 
-//	driver = new FirefoxDriver();
-	System.setProperty("webdriver.chrome.driver", "/home/marvin/Documents/Selinium/chromedriver");
-	driver = new ChromeDriver();
-	baseUrl = "http://172.16.0.12:8589";
-	driver.manage().window().maximize();
-	driver.get(baseUrl + "/#/login");
-	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	public void setBrowser() {
+		browser = "Firefox";
 	}
-	  
-@Test
+	public void browserConfig () {
+
+		if (browser.contains("Firefox")) {
+			System.setProperty("webdriver.gecko.driver","/home/marvin/git/VENTAPOS/Venta/libs/chromeDriver/geckodriver"); 
+			driver = new FirefoxDriver();
+		}else if (browser.contains("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", "/home/marvin/git/VENTAPOS/Venta/libs/chromeDriver/chromedriver");
+			driver = new ChromeDriver();
+		}
+	}
+
+	@Test
 	public void createItemTest() throws Exception {
-	toAdmLogin();
-	itemsHomeBtn();
-	createNewItemBtn();
-	Thread.sleep(1000);
-	setItemDetails();
-	toSaveItem();
-	Thread.sleep(1000);
-	  String str = driver.findElement(By.xpath(getItemSuccess)).getText();
-	  if (str.contains("Successfully")) {
+		toAdmLogin();
+		itemsHomeBtn();
+		createNewItemBtn();
+		Thread.sleep(1000);
+		setItemDetails();
+		toSaveItem();
+		Thread.sleep(1000);
+		String str = driver.findElement(By.xpath(getItemSuccess)).getText();
+		if (str.contains("Successfully")) {
 			System.out.println("Create Item Test = Passed!");
 		} else {
 			System.out.println("Create Item Test = Failed!");
 		}
-}
+	}
 
-@Test
+	@Test
 	public void emptyItemCodeFieldTest() throws Exception {
-	toAdmLogin();
-	itemsHomeBtn();
-	createNewItemBtn();
-	Thread.sleep(1000);
-	setItemDetails();
-	driver.findElement(By.xpath(itemCodeField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-	driver.findElement(By.xpath(itemCodeField)).sendKeys(Keys.BACK_SPACE);
-	toSaveItem();
-	Thread.sleep(1000);
-	String str = driver.findElement(By.xpath(getItemError)).getText();
-	if (str.contains("Please review fields again."))
-	{
-		System.out.println("Empty Item Code Field Test = Passed!");
-	} else {
-		System.out.println("Empty Item Code Field Test = Failed!");
-	}
-}
-
-@Test
-	public void emptyItemDescFieldTest() throws Exception {
-	toAdmLogin();
-	itemsHomeBtn();
-	createNewItemBtn();
-	Thread.sleep(1000);
-	setItemDetails();
-	driver.findElement(By.xpath(itemDescField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-	driver.findElement(By.xpath(itemDescField)).sendKeys(Keys.BACK_SPACE);
-	toSaveItem();
-	Thread.sleep(1000);
-	String str = driver.findElement(By.xpath(getItemError)).getText();
-	if (str.contains("Please review fields again."))
-	{
-		System.out.println("Empty Item Description Field Test = Passed!");
-	} else {
-		System.out.println("Empty Item Description Field Test = Failed!");
-	}
-}
-
-@Test
-	public void emptyItemBarcodeFieldTest() throws Exception {
-	toAdmLogin();
-	itemsHomeBtn();
-	createNewItemBtn();
-	Thread.sleep(1000);
-	setItemDetails();
-	driver.findElement(By.xpath(barcodeField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-	driver.findElement(By.xpath(barcodeField)).sendKeys(Keys.BACK_SPACE);
-	toSaveItem();
-	Thread.sleep(1000);
-	String str = driver.findElement(By.xpath(getItemError)).getText();
-	if (str.contains("Please review fields again."))
-	{
-		System.out.println("Empty Item Barcode Field Test = Passed!");
-	} else {
-		System.out.println("Empty Item Barcode Field Test = Failed!");
-	}
-}
-
-@Test
-	public void vatableItemSwitchOffTest() throws Exception {
-	toAdmLogin();
-	itemsHomeBtn();
-	createNewItemBtn();
-	Thread.sleep(1000);
-	setItemDetails();
-	driver.findElement(By.xpath(vatableSwitch)).click();
-	toSaveItem();
-	Thread.sleep(1000);
-	String str = driver.findElement(By.xpath(getItemSuccess)).getText();
-	if (str.contains("Successfully created item."))
-	{
-		System.out.println("Vatable Item Switch Off Test = Passed!");
-	} else {
-		System.out.println("Vatable Item Switch Off Test = Failed!");
-	}
-}
-public void noDiscountApplyTest() throws Exception{
-	toAdmLogin();
-	itemsHomeBtn();
-	createNewItemBtn();
-	Thread.sleep(1000);
-	setItemDetails();
-	driver.findElement(By.xpath(discountField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-	driver.findElement(By.xpath(discountField)).sendKeys(Keys.BACK_SPACE);
-	toSaveItem();
-	Thread.sleep(1000);
-	String str = driver.findElement(By.xpath(getItemSuccess)).getText();
-	if (str.contains("Successfully created item."))
-	{
-		System.out.println("No Discount Apply Test = Passed!");
-	} else {
-		System.out.println("No Discount Apply Test = Failed!");
-	}
-}
-
-private void toSaveItem() {
-	driver.findElement(By.xpath(saveItemBtn)).click();
-}
-
-private void createNewItemBtn() {
-	driver.findElement(By.xpath(createItemBtn)).click();
-}
-
-public void setItemDetails() {
-	driver.findElement(By.xpath(itemCodeField)).sendKeys(RandomStringUtils.randomAlphanumeric(3));
-	driver.findElement(By.xpath(itemDescField)).sendKeys(RandomStringUtils.randomAlphanumeric(10));
-	driver.findElement(By.xpath(originalPriceField)).sendKeys(RandomStringUtils.randomNumeric(3));
-	driver.findElement(By.xpath(barcodeField)).sendKeys(RandomStringUtils.randomNumeric(6));
-	driver.findElement(By.xpath(vatableSwitch)).click();
-	driver.findElement(By.xpath(discountField)).sendKeys(RandomStringUtils.randomNumeric(2));
-	}
-
-private void itemsHomeBtn()throws Exception {
-	driver.findElement(By.xpath(itemsMngtHomeBtn)).click();
+		toAdmLogin();
+		itemsHomeBtn();
+		createNewItemBtn();
 		Thread.sleep(1000);
-}
-	
+		setItemDetails();
+		driver.findElement(By.xpath(itemCodeField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		driver.findElement(By.xpath(itemCodeField)).sendKeys(Keys.BACK_SPACE);
+		toSaveItem();
+		Thread.sleep(1000);
+		String str = driver.findElement(By.xpath(getItemError)).getText();
+		if (str.contains("Please review fields again."))
+		{
+			System.out.println("Empty Item Code Field Test = Passed!");
+		} else {
+			System.out.println("Empty Item Code Field Test = Failed!");
+		}
+	}
 
-private void toAdmLogin() {
-	driver.findElement(By.xpath(emailField)).sendKeys(authAdm);
-    driver.findElement(By.xpath(passwordField)).sendKeys(authAdmPassword);
-    driver.findElement(By.xpath(btnLogin)).click();
-}
+	@Test
+	public void emptyItemDescFieldTest() throws Exception {
+		toAdmLogin();
+		itemsHomeBtn();
+		createNewItemBtn();
+		Thread.sleep(1000);
+		setItemDetails();
+		driver.findElement(By.xpath(itemDescField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		driver.findElement(By.xpath(itemDescField)).sendKeys(Keys.BACK_SPACE);
+		toSaveItem();
+		Thread.sleep(1000);
+		String str = driver.findElement(By.xpath(getItemError)).getText();
+		if (str.contains("Please review fields again."))
+		{
+			System.out.println("Empty Item Description Field Test = Passed!");
+		} else {
+			System.out.println("Empty Item Description Field Test = Failed!");
+		}
+	}
 
-@After
-public void tearDown() throws Exception {
-  driver.quit();
- 
+	@Test
+	public void emptyItemBarcodeFieldTest() throws Exception {
+		toAdmLogin();
+		itemsHomeBtn();
+		createNewItemBtn();
+		Thread.sleep(1000);
+		setItemDetails();
+		driver.findElement(By.xpath(barcodeField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		driver.findElement(By.xpath(barcodeField)).sendKeys(Keys.BACK_SPACE);
+		toSaveItem();
+		Thread.sleep(1000);
+		String str = driver.findElement(By.xpath(getItemError)).getText();
+		if (str.contains("Please review fields again."))
+		{
+			System.out.println("Empty Item Barcode Field Test = Passed!");
+		} else {
+			System.out.println("Empty Item Barcode Field Test = Failed!");
+		}
+	}
+
+	@Test
+	public void vatableItemSwitchOffTest() throws Exception {
+		toAdmLogin();
+		itemsHomeBtn();
+		createNewItemBtn();
+		Thread.sleep(1000);
+		setItemDetails();
+		driver.findElement(By.xpath(vatableSwitch)).click();
+		toSaveItem();
+		Thread.sleep(1000);
+		String str = driver.findElement(By.xpath(getItemSuccess)).getText();
+		if (str.contains("Successfully created item."))
+		{
+			System.out.println("Vatable Item Switch Off Test = Passed!");
+		} else {
+			System.out.println("Vatable Item Switch Off Test = Failed!");
+		}
+	}
+	public void noDiscountApplyTest() throws Exception{
+		toAdmLogin();
+		itemsHomeBtn();
+		createNewItemBtn();
+		Thread.sleep(1000);
+		setItemDetails();
+		driver.findElement(By.xpath(discountField)).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		driver.findElement(By.xpath(discountField)).sendKeys(Keys.BACK_SPACE);
+		toSaveItem();
+		Thread.sleep(1000);
+		String str = driver.findElement(By.xpath(getItemSuccess)).getText();
+		if (str.contains("Successfully created item."))
+		{
+			System.out.println("No Discount Apply Test = Passed!");
+		} else {
+			System.out.println("No Discount Apply Test = Failed!");
+		}
+	}
+
+	private void toSaveItem() {
+		driver.findElement(By.xpath(saveItemBtn)).click();
+	}
+
+	private void createNewItemBtn() {
+		driver.findElement(By.xpath(createItemBtn)).click();
+	}
+
+	public void setItemDetails() {
+		driver.findElement(By.xpath(itemCodeField)).sendKeys(RandomStringUtils.randomAlphanumeric(3));
+		driver.findElement(By.xpath(itemDescField)).sendKeys(RandomStringUtils.randomAlphanumeric(10));
+		driver.findElement(By.xpath(originalPriceField)).sendKeys(RandomStringUtils.randomNumeric(3));
+		driver.findElement(By.xpath(barcodeField)).sendKeys(RandomStringUtils.randomNumeric(6));
+		driver.findElement(By.xpath(vatableSwitch)).click();
+		driver.findElement(By.xpath(discountField)).sendKeys(RandomStringUtils.randomNumeric(2));
+	}
+
+	private void itemsHomeBtn()throws Exception {
+		driver.findElement(By.xpath(itemsMngtHomeBtn)).click();
+		Thread.sleep(1000);
+	}
+
+
+	private void toAdmLogin() {
+		driver.findElement(By.xpath(emailField)).sendKeys(authAdm);
+		driver.findElement(By.xpath(passwordField)).sendKeys(authAdmPassword);
+		driver.findElement(By.xpath(btnLogin)).click();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+
 	}
 }
 
